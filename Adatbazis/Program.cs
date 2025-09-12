8b53cd4a01c55e9bea2e90146c8f6b8ba26bfc10
SzabolcsBodi756 <bodisz@kkszki.hu>
2025-09-12 10:42:00 +0200

    connect class is ready

---
+++ Adatbazis/Program.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Adatbazis
+{
+
+    public class Connect
+
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string RelesDate { get; set; }
+
+        public Connect(int id, string title, string author, string relesDate)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            RelesDate = relesDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Adatok: {Id}, {Title}, {Author}, {RelesDate}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<Connect> lista = new List<Connect>();
+
+            MySqlConnection kapcs = new MySqlConnection("server = server.fh2.hu;database = v2labgwj_12a; uid = v2labgwj_12a; password = 'HASnEeKvbDEPGgvTZubG'");
+
+            kapcs.Open();
+            // lekérdezés - SELECT
+            var lekerdezes = new MySqlCommand("select * from konyv, szerzo where konyv.szerzoid = szerzo.id", kapcs).ExecuteReader();
+            while (lekerdezes.Read())
+            {
+                lista.Add(new Connect(lekerdezes.GetInt32(0), lekerdezes.GetString(1), lekerdezes.GetString(2), lekerdezes.GetString(3)));
+            }
+            lekerdezes.Close();
+            kapcs.Close();
+
+            foreach (Connect connect in lista)
+            {
+                Console.WriteLine(connect.ToString());
+                
+            }*/
+        }
+    }
+}
